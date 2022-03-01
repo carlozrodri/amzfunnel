c40994561cfc96c7c3804bea53dcffdda2fc11b5
carlozrodri <patica100@gmail.com>
2022-03-01 17:50:13 +0000

    test

--- amzfunnel/settings.py
+++ amzfunnel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f&3o3hf)m)kf0hp-zs2zvab7-u4tx_rmu*s$y)g*-!5u+t)9q9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','127.0.0.1',".herokuapp.com"]
 
