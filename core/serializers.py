3fda30eeec8f00dc2f56acd3ce0099f6994bfcdf
carlozrodri <patica100@gmail.com>
2022-03-02 18:52:28 +0000

    serlializer fix

--- core/serializers.py
+++ core/serializers.py
@@ -1,25 +1,24 @@
+from pyexpat import model
 from rest_framework import serializers
 from .models import ItemSizer
 
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    item_pictures = serializers.CharField(max_length=500, default='')
-    item_description = serializers.CharField(max_length=100, default='')
-    url_amazon = serializers.CharField(max_length=500, default='')
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ItemSizer
+        fields = ['id', "title", "item_pictures", "item_description", "url_amazon"]
 
 
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return ItemSizer.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     """
+    #     Create and return a new `Snippet` instance, given the validated data.
+    #     """
+    #     return ItemSizer.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     """
+    #     Update and return an existing `Snippet` instance, given the validated data.
+    #     """
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.save()
+    #     return instance
