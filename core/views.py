b5968bd26bcb8f06416d3ebad945005b5487e8d8
Carlos Rodriguez <patica100@gmail.com>
2022-03-14 23:36:03 +0000

    fix

--- core/views.py
+++ core/views.py
@@ -1,14 +1,14 @@
 from .serializers import SnippetSerializer
-from .models import ItemSizer
+from .models import ItemSizer, Categorias
 from rest_framework import generics
 from rest_framework import filters
 
 
 
 class QuestionsAPIView(generics.ListCreateAPIView):
-    queryset = ItemSizer.objects.all()
+    queryset = Categorias.objects.all()
     serializer_class = SnippetSerializer
-    search_fields = ['title']
+    search_fields = ['category__name']
     filter_backends = (filters.SearchFilter,)
     queryset = ItemSizer.objects.all()
     serializer_class = SnippetSerializer
