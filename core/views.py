a98228a5e30f443a68740551467d0d2680114185
usama <usamahaide0786@gmail.com>
2022-03-16 17:25:14 +0000

    fixed

--- core/views.py
+++ core/views.py
@@ -16,4 +16,6 @@ class QuestionsAPIView(generics.ListCreateAPIView):
 
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
+    search_fields = ['name']
+    filter_backends = (filters.SearchFilter,)
     queryset = Categorias.objects.all()
