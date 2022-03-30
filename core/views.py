2a03945baaad7933c1311ea55ea8c58292a0fe35
Carlos Rodriguez <patica100@gmail.com>
2022-03-30 22:55:53 +0100

    views readonly update

--- core/views.py
+++ core/views.py
@@ -1,21 +1,18 @@
 from .serializers import SnippetSerializer, CategorySerializer
 from .models import ItemSizer, Categorias
-from rest_framework import generics
-from rest_framework import filters
-from rest_framework import viewsets
+from rest_framework import generics, filters, viewsets
 
 
-class QuestionsAPIView(generics.ListCreateAPIView):
+class QuestionsAPIView(generics.ListAPIView):
     queryset = Categorias.objects.all()
     serializer_class = SnippetSerializer
     search_fields = ['category__name']
     filter_backends = (filters.SearchFilter,)
     queryset = ItemSizer.objects.all()
-    serializer_class = SnippetSerializer
 
-
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
     search_fields = ['name']
     filter_backends = (filters.SearchFilter,)
     queryset = Categorias.objects.all()
+  
